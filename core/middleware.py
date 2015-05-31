b66a3a8d3705a0cfcd9dc79a0b7c2a26cf49ade5
Łukasz Lechowicz <llechowicz@gmail.com>
2015-05-31 11:32:38 +0100

    Login is enforced through app.yaml file.

--- core/middleware.py
+++ core/middleware.py
@@ -1,25 +1,16 @@
 from google.appengine.api import users
 
-from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.http import HttpResponseRedirect
 
 
 class LoginRequiredMiddleware(object):
 
     def process_view(self, request, view_func, view_args, view_kwargs):
         user = users.get_current_user()
+        email = user.email()
 
-        if not user:
-            if request.META.get('HTTP_X_APPENGINE_CRON', False):
-                return None
-            else:
-                return HttpResponseRedirect(
-                    users.create_login_url(request.get_full_path()))
-        else:
-            email = user.email()
-
-            if not email.endswith('@potatolondon.com'):
-                return HttpResponseForbidden()
-
-            request.user = user
+        if not email.endswith('@potatolondon.com'):
+            return HttpResponseRedirect(
+                users.create_login_url(request.get_full_path()))
 
         return view_func(request, *view_args, **view_kwargs)
